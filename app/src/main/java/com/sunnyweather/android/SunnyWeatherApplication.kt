3c4216be16e0e22e9d6d7ef7b0110145e0630dbc
Tangwh <846765179@qq.com>
2021-12-27 22:24:02 +0800

    网络模块加入，图片文件加入。

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context:Context
+
+        const val TOKEN = "令牌值"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
